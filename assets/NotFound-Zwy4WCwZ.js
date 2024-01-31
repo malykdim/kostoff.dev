4c2e60979636eee12935d34bece651e7bd3c6789
Albena Kostova <albena.kostov@gmail.com>
2024-01-31 13:59:54 +0100

    Updates

---
+++ assets/NotFound-Zwy4WCwZ.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as t,p as s,a,b as n}from"./index-GLIaqbg2.js";const d={},_=o=>(s("data-v-24ce2e0d"),o=o(),a(),o),p={class:"not-found"},r=_(()=>n("h2",null,"not found",-1)),f=[r];function i(o,u){return c(),t("section",p,f)}const l=e(d,[["render",i],["__scopeId","data-v-24ce2e0d"]]);export{l as default};
