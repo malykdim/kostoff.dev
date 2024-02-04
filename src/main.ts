19058e8f3aac27d817e5e2526e56e674eb478fde
Albena Kostova <albena.kostov@gmail.com>
2024-02-04 10:43:53 +0100

    Adding Admin Store & Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 import router from './config/router.ts';
 
 
 
 
-import './style.scss'
-import App from './App.vue'
+import './style.scss';
+import App from './App.vue';
 
-import AppHeaderVue from './components/header/AppHeader.vue'
-import AppFooterVue from './components/footer/AppFooter.vue'
+import AppHeaderVue from './components/header/AppHeader.vue';
+import AppFooterVue from './components/footer/AppFooter.vue';
 
-const app = createApp(App)
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(router)
+app.use(pinia);
+app.use(router);
 
 app.component('AppHeader', AppHeaderVue)
-    .component('AppFooter', AppFooterVue)
+    .component('AppFooter', AppFooterVue);
 
-app.mount('#app')
+app.mount('#app');
