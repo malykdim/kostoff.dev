f2ca1a4ec505eda28ad47059f657dc0b4899cf38
Albena Kostova <albena.kostov@gmail.com>
2024-11-03 15:19:18 +0100

    Changing routes

--- src/config/router.ts
+++ src/config/router.ts
@@ -6,31 +6,20 @@ const Contact = () => import('../views/Contact.vue');
 const Projects = () => import('../views/projects/Projects.vue');
 const Details = () => import('../views/details/ProjectDetails.vue');
 
-// const routes = [
-//   { path: '/', name: 'home', component: Home },
-//   { path: '/projects', name: 'projects', component: Projects },
-//   { path: '/projects/:id', name: 'details', component: Details, props: true },
-//   { path: '/contact', name: 'contact', component: Contact },
-//   {
-//     path: '/:pathMatch(.*)*',
-//     name: 'NotFound',
-//     component: NotFound,
-//   },
-// ];
 const routes = [
-  { path: '/kostoff.dev/', name: 'home', component: Home },
-  { path: '/kostoff.dev/projects', name: 'projects', component: Projects },
-  { path: '/kostoff.dev/projects/:id', name: 'details', component: Details, props: true },
-  { path: '/kostoff.dev/contact', name: 'contact', component: Contact },
+  { path: '/', name: 'home', component: Home },
+  { path: '/projects', name: 'projects', component: Projects },
+  { path: '/projects/:id', name: 'details', component: Details, props: true },
+  { path: '/contact', name: 'contact', component: Contact },
   {
-    path: '/kostoff.dev/:pathMatch(.*)*',
+    path: '/:pathMatch(.*)*',
     name: 'NotFound',
     component: NotFound,
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory('/kostoff.dev/'),
+  history: createWebHistory('#'),
   routes,
 });
 
